1a7d33781593834b3c3234af66d5bbefd4397383
abdallah <boodykhattab97@gmail.com>
2025-02-07 15:00:31 +0200

    migration

---
+++ app/Observers/TravelObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Travel;
+
+class TravelObserver
+{
+    /**
+     * Handle the Travel "creating" event.
+     */
+    public function creating(Travel $travel): void
+    {
+    //  $travel->slug = Str::slug($travel->name) .'-' . uniqid();
+      $travel->slug = str($travel->name)->slug() . '-' . uniqid();
+    }
+
+   
+}
