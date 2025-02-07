408bd37af10a3cb07124438f6240a7b910bb2a6d
abdallah <boodykhattab97@gmail.com>
2025-02-07 15:30:09 +0200

    uuid

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,18 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory , HasUuids;
 
     protected $table = 'travels';
 
-    protected $fillable = ['is_public',
-    'name',
-    'slug',
-    'description',
-    'number_of_days'];
+    protected $fillable = [
+        'is_public',
+        'name',
+        'slug',
+        'description',
+        'number_of_days'
+    ];
+
+    public function tours(): HasMany{
+        return $this->hasMany(Tour::class);
+    }
+
+
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value,$attributes) => $attributes['number_of_days'] -1
+        );
+    }
+
+    /*
+    public function getNumberOfNightsAttribute(){
+        return $this->number_of_days - 1;
+    }*/
 }
